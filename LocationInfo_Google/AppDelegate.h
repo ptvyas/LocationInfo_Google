7896ee165a751cd9a4f3708dcfb377944267835c
PT Vyas <vyasbhagat@gmail.com>
2017-08-04 16:12:48 +0530

    Initial Commit

---
+++ LocationInfo_Google/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationInfo_Google
+//
+//  Created by WOS_iMac_2 on 8/4/17.
+//  Copyright © 2017 Vyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationInfo_Google/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LocationInfo_Google
+//
+//  Created by WOS_iMac_2 on 8/4/17.
+//  Copyright © 2017 Vyas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
